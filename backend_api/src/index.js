a504c97bb0dd1500db7ab0c21c8d92529314b770
fernandopulle <yohan@softvilmedia.com>
2021-10-04 14:47:09 +0530

    minor changes

--- backend_api/src/index.js
+++ backend_api/src/index.js
@@ -6,7 +6,7 @@ const emitter = new EventEmitter()
 
 emitter.on('db.ok', (db)=>{
     console.log('database connected')
-
+    
     return server.start({port: config.serverSettings.port}).then(
         app =>{
             console.log(`Server started succesfully, running on port:${config.serverSettings.port}`)
